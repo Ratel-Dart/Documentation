0b4390d1e09a1f716beb6002bd18e8d50a223b2f
DanielBrisch <danielbrisch157@gmail.com>
2025-05-04 23:41:55 -0300

    chore: update config.ts on baseURL

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   url: 'https://ratel-dart.github.io/',
-  baseUrl: '/Documentation  ',
+  baseUrl: '/Documentation',
   projectName: 'Documentation',
   trailingSlash: false,
   organizationName: 'DanielBrisch',
